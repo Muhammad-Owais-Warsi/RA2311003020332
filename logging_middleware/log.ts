5d677b2412864bf8e9fa2f1e9559c24af8fccd45
Muhammad-Owais-Warsi <warsimuhammadowais@gmail.com>
2026-05-02 07:17:11 +0100

    fix: directly return repsonse in logging middleware instead of returning promise

--- logging_middleware/log.ts
+++ logging_middleware/log.ts
@@ -1,30 +1,29 @@
 import { type Log } from "./types";
 
-export default async function Log(log: Log) {
-    try {
-        const result = await fetch(`${process.env.TEST_SERVER_BASE}/logs`, {
-            method: "POST",
-            headers: {
-                Authorization: `Bearer ${process.env.TOKEN}`,
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(log),
+export default function Log(log: Log) {
+    return fetch(`${process.env.TEST_SERVER_BASE}/logs`, {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${process.env.TOKEN}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(log),
+    })
+        .then(async (result) => {
+            if (result.ok) {
+                return { message: "Success: Log registered successfully" };
+            }
+            const errorData = await result.text();
+            return {
+                error: `Failed to send log. Status: ${result.status}`,
+                details: errorData,
+            };
+        })
+        .catch((e) => {
+            return {
+                error: `Internal server error. Please try later. ${e.message}`,
+            };
         });
-
-        if (result.ok) {
-            return { message: "Success: Log registered successfully" };
-        }
-
-        const errorData = await result.text();
-        return {
-            error: `Failed to send log. Status: ${result.status}`,
-            details: errorData,
-        };
-    } catch (e) {
-        return {
-            error: `Internal server error. Please try later. ${e.message}`,
-        };
-    }
 }
 
 //debugging
